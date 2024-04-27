c3e99173ab65f98521256ea67691ab3e5c3aaa9e
MSCV2607 <maurosebavecc@gmail.com>
2024-04-27 17:22:55 -0300

    Actualizacion agrega desarollo por consola POO

---
+++ CONCEPTOSPOO/CONCEPTOSPOO/PUNTO.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CONCEPTOSPOO
+{
+    internal class PUNTO
+    {
+
+        public PUNTO(int x, int y)
+        {
+            //Console.WriteLine($"Cordenada x: {x}, cordenada y: {y}");
+            this.x = x;
+            this.y = y;
+            contadorDeObjetos++;
+        }
+
+        public PUNTO()
+        {
+            this.x = 0;
+            this.y = 0;
+            contadorDeObjetos++;
+        }
+
+        public double distancia(PUNTO otroPunto)
+        {
+
+            int xDif = this.x - otroPunto.x;
+            int ydif = this.y - otroPunto.y;
+
+            double distanciaPuntos = Math.Sqrt(Math.Pow(xDif, 2)+Math.Pow(ydif, 2));
+
+            return distanciaPuntos;
+
+        }
+
+        private int x, y;
+
+        private static int contadorDeObjetos = 0;
+
+        public static int contadorObj()
+        {
+            return contadorDeObjetos;
+        }
+
+    }
+}
---
+++ CONCEPTOSPOO/CONCEPTOSPOO/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//IMPORTANTE PARA NO PONER MATH A CADA RATO
+using static System.Math;
+
+using static System.Console; //EN VEZ DE ESCRIBIR "Consol.WriteLine("")" ESCRIBIMOS "WriteLine("")"
+
+namespace CONCEPTOSPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //realizarTarea();
+
+            double raiz = Sqrt(9);
+            double potencia = Math.Pow(3, 4);
+
+            Console.WriteLine(raiz);
+            Console.WriteLine(potencia);
+
+            //ESTO ES UNA CLASE ANONIMA 
+            var MiVariable = new { Nombre = "Juan", Edad = 18 };
+            Console.WriteLine(MiVariable.Nombre + " " + MiVariable.Edad);
+
+        }
+
+        static void realizarTarea()
+        {
+            PUNTO origen = new PUNTO();
+            PUNTO destino = new PUNTO(150, 90);
+            
+            double distancia = origen.distancia(destino);
+            Console.WriteLine($"La distancia entre dos puntos es de: {distancia}");
+
+            Console.WriteLine($"Numero de objetos creados: {PUNTO.contadorObj()}");
+
+            //TODO: 
+            //con todo indiacmos que hay que continuar desde aca el codigo
+        }
+
+    }
+}
