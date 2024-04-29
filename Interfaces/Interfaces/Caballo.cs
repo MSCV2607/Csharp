d0718f9713ee88da0293c04fbdc8592e5053b24d
MSCV2607 <maurosebavecc@gmail.com>
2024-04-29 19:03:54 -0300

    04.29 INTERFACES

---
+++ Interfaces/Interfaces/Caballo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interfaces
+{
+    //ESTA CLASE SI IMPLEMENTA INTERFACE
+    //ASI SE HACE CON LA , Y CON EL NOMBRE DE LA INTERFACE DESPUES DE LA CLASE HEREDADA VA
+    internal class Caballo : Mamifero , IMamiferosTerrestres
+    {
+        public Caballo(string nombre) : base(nombre)
+        {
+        }
+
+        //METODO DE LA INTERFAZ
+        public int numPatas()
+        {
+            return 4;
+        }
+
+        public void galopar()
+        {
+            Console.WriteLine("Soy capaz de galopar");
+        }
+    }
+}
